07760a442c879f6ba80b882bd095a4d3efe768ce
devin lacey <devinolacey@gmail.com>
2024-04-02 10:20:53 -0700

    Ian fix Auth

--- api/queries/accounts.py
+++ api/queries/accounts.py
@@ -2,17 +2,13 @@ from pydantic import BaseModel
 from queries.pool import pool
 from typing import List, Union, Optional
 from fastapi import HTTPException
+from pydantic import BaseModel
+from queries.pool import pool
+from typing import List, Union, Optional
+from fastapi import HTTPException
 
 # from PIL import Image
 # import secrets
-
-
-class UserIn(BaseModel):
-    email: str
-    password: str
-    password_confirmation: str
-    riot_id: str
-    u_gg: str
 
 
 class UserOut(BaseModel):
@@ -46,32 +42,38 @@ class Error(BaseModel):
     message: str
 
 
+class UserIn(BaseModel):
+    username: str
+    password: str
+    password_confirmation: str
+    riot_id: str
+    u_gg: str
+
+
 class AccountsRepository(BaseModel):
     def create_account(self, users: UserIn, hashed_password: str) -> UserOut:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
-                    db.execute(
+                    result = db.execute(
                         """
-                        INSERT INTO accounts (
-                            email,
-                            hash_password,
-                            riot_id,
-                            u_gg)
-                        VALUES (%s, %s, %s, %s)
+                        INSERT INTO accounts
+                            (email, hash_password, riot_id, u_gg)
+                        VALUES
+                            (%s,%s,%s,%s)
                         RETURNING id;
                         """,
                         [
-                            users.email.lower(),
+                            users.username.lower(),
                             hashed_password,
                             users.riot_id,
                             users.u_gg,
                         ],
                     )
-                    user_id = db.fetchone()[0]
+                    user_id = result.fetchone()[0]
                     return UserOutWithPassword(
                         id=user_id,
-                        username=users.email,
+                        username=users.username,
                         riot_id=users.riot_id,
                         u_gg=users.u_gg,
                         hashed_password=hashed_password,
@@ -80,7 +82,7 @@ class AccountsRepository(BaseModel):
             print(e)
             raise DuplicateAccountError from e
 
-    def get_accounts(self) -> Union[List[UserOut], Error]:
+    def get_all_accounts(self) -> Union[List[UserOut], Error]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
@@ -90,6 +92,7 @@ class AccountsRepository(BaseModel):
                         FROM accounts;
                         """
                     )
+                    print
                     return [self.record_to_user_out(record) for record in result]
         except Exception as e:
             print(e)
@@ -100,7 +103,7 @@ class AccountsRepository(BaseModel):
     def record_to_user_out(self, record):
         return UserOut(
             id=record[0],
-            email=record[1],
+            username=record[1],
             riot_id=record[2],
             u_gg=record[3],
         )
@@ -111,27 +114,23 @@ class AccountsRepository(BaseModel):
                 with conn.cursor() as db:
                     db.execute(
                         """
-                        SELECT id, email, hash_password, riot_id, u_gg
-                        FROM accounts
-                        WHERE email = %s;
-                        """,
-                        [email],
+                            SELECT id, email, riot_id, u_gg, hash_password
+                            FROM accounts
+                            WHERE email = %s;
+                            """,
+                        [email.lower()],
                     )
                     record = db.fetchone()
                     if record is None:
-                        raise HTTPException(
-                            status_code=404, detail="Account does not exist"
-                        )
+                        return "fuck"
                     return UserOutWithPassword(
                         id=record[0],
                         username=record[1],
-                        hashed_password=record[2],
-                        riot_id=record[3],
-                        u_gg=record[4],
+                        riot_id=record[2],
+                        u_gg=record[3],
+                        hashed_password=record[4],
                     )
+
         except Exception as e:
             print(e)
-            print("-------------Test")
-            raise HTTPException(
-                status_code=400, detail="Could not fetch account"
-            ) from e
+            return {"message": "Account not found"}
--- api/routers/accounts.py
+++ api/routers/accounts.py
@@ -23,6 +23,7 @@ from queries.accounts import (
 )
 
 from typing import Union, List
+from fastapi import HTTPException, status
 
 
 class AccountForm(BaseModel):
@@ -57,21 +58,49 @@ async def create_account(
     repo: AccountsRepository = Depends(),
 ):
     if info.password != info.password_confirmation:
-        raise HTTPException(status_code=400, detail="Passwords do not match")
+        raise HTTPException(
+            status_code=400,
+            detail="Passwords do not match",
+        )
     hashed_password = authenticator.hash_password(info.password)
     try:
         account = repo.create_account(info, hashed_password)
     except DuplicateAccountError:
         raise HTTPException(
-            status_code=400, detail="Account with this email already exists"
+            status_code=400,
+            detail="An Account already exists with these credentials",
         )
-    form = AccountForm(username=info.email, password=info.password)
+    form = AccountForm(username=info.username, password=info.password)
     token = await authenticator.login(response, request, form, repo)
     return AccountToken(account=account, **token.dict())
 
 
-@router.get("/accounts", response_model=Union[Error, List[UserOutWithPassword]])
+@router.get("/accounts", response_model=Union[Error, List[UserOut]])
 def get_accounts(
     repo: AccountsRepository = Depends(),
 ):
-    return repo.get_accounts()
+    return repo.get_all_accounts()
+
+
+@router.get("/token", response_model=AccountToken | None)
+async def get_token(
+    request: Request,
+    account: dict = Depends(authenticator.try_get_current_account_data),
+) -> AccountToken | None:
+    if account and authenticator.cookie_name in request.cookies:
+        return {
+            "access_token": request.cookies[authenticator.cookie_name],
+            "type": "Bearer",
+            "account": account,
+        }
+
+
+@router.get("/account", response_model=Union[Error, UserOutWithPassword])
+def get_account(
+    email: str,
+    repo: AccountsRepository = Depends(),
+):
+    result = repo.get_account(email)
+    if result == "fuck":
+        raise HTTPException(status_code=404, detail="Account not found")
+    return result
